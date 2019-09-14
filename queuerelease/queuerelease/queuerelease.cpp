cb535715d27d73a099c4f0760229fe8f25df878f
chilya-coder <52803366+chilya-coder@users.noreply.github.com>
2019-09-14 19:14:38 +0300

    Update queuerelease.cpp
    Final version

--- queuerelease/queuerelease/queuerelease.cpp
+++ queuerelease/queuerelease/queuerelease.cpp
@@ -1,22 +1,20 @@
-﻿#include <iostream>
-#include <stdlib.h>
-#include <time.h>
+#include <iostream>
+#include <ctime>
 #include <queue>
 #include <cmath>
 using namespace std;
 
-bool check(int a) { // check if number is prime one
-	for (int i = 2; i < a; i++)
+bool check(int a) { 
+	if (a == 1)
+		return false; // 1 is not a prime number
+	for (int start = 2; start * start <= a; start++)
 	{
-		float n = static_cast<float> (a) / i;
-		if (a / i == n) {
+		if (a % start == 0)
 			return false;
-		}
-		else {
-			return true;
-		}
 	}
-}
+	return true;
+	}
+
 int main()
 {
 	queue <int> myQueue; // creating a queue with 100 random numbers
@@ -24,27 +22,34 @@ int main()
 	cout << "Numbers in a queue: ";
 	for (int i = 0; i < 70; ++i)
 	{
-		int secret = rand() % 100 + 1;
+		int secret = rand() % 100 + 1; // random number from 1 to 100
 		myQueue.emplace(secret);
 		cout << myQueue.back() << " ";
-		
+
 	}
+
+	/*int* a, * b; // Это не сработает, потому что очередь - динамический массив, т.е.
+	a = &myQueue.front();
+	b = &myQueue.back();
+	while (a <= b)
+	{
+		cout << *a << " ";
+		a++;
+	}
+	*/ 
+
+
 	cout << "\nPrime numbers: ";
 
+	
 	while (!myQueue.empty())
 	{
-		check(myQueue.front());
 
-		if (check)
+		if (check(myQueue.front()))
 		{
 			cout << myQueue.front() << " ";
-			myQueue.pop();
 		}
-		else {
-			myQueue.pop();
-		}
+		myQueue.pop();
 	}
-
-	return 0;	
+	return 0;
 }
-
